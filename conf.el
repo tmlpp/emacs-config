2979de9c85215c2b825bcdb4e5d6d5cd0dce6128
tmlpp <teemu.lappi@gmail.com>
2018-01-04 19:02:18 +0200

    j/k movement in org-agenda

--- conf.el
+++ conf.el
@@ -230,6 +230,9 @@
 
 (define-key evil-normal-state-map [return] 'tsl/evil-insert-line-below)
 
+(define-key org-agenda-mode-map "j" 'evil-next-line)
+(define-key org-agenda-mode-map "k" 'evil-previous-line)
+
 (setq key-chord-two-keys-delay 0.5)
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
 ; (key-chord-define evil-normal-state-map "ol" 
