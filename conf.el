2e64eda63a34003c73962ca440c09c51f97e02bc
tmlpp <teemu.lappi@gmail.com>
2018-01-20 04:32:13 +0200

    new org-mode repository

--- conf.el
+++ conf.el
@@ -15,8 +15,8 @@
 
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (package-initialize)
 (add-to-list 'load-path
              "~/.emacs.d/elisp")
